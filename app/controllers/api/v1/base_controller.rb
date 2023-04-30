90da32a023acf7b18ad1211d4125549421e7d9f8
kitadeshogo <tori3227@gmail.com>
2023-04-30 23:32:34 +0900

    ルーティングを設定 #4

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::BaseController < ApplicationController
   include Api::ExceptionHandler
   include FirebaseAuth
 
-  befer_action :authenticate_token
+  before_action :authenticate_token
 
   def authenticate_token
     authenticate_with_http_token do |token, _options|
