251b9b9d1c246c855ab3f914a6274a1e05345f47
kitadeshogo <tori3227@gmail.com>
2023-05-21 22:06:39 +0900

    CreateFolderのマイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_29_205909) do
+ActiveRecord::Schema.define(version: 2023_05_12_200453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "folders", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_folders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "uid", null: false
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2023_04_29_205909) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "folders", "users"
 end
