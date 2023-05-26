807708ba0c607de0b88a21abc4f59302367d6df3
kitadeshogo <tori3227@gmail.com>
2023-05-26 22:32:49 +0900

    folderを返さない問題の解決

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -8,18 +8,19 @@ class Api::V1::BaseController < ApplicationController
   def authenticate_token
     authenticate_with_http_token do |token, _options|
 
-        result = verify_id_token(token)
-
-        if result[:errors]
-            render_400(nil, result[:errors])
-        else
-            @_current_user = User.find_or_create_by!(uid: result[:uid])
-        end
+      result = verify_id_token(token)
+      Rails.logger.info "token検証実行結果: #{result}"
+      
+      if result[:errors]
+        render_400(nil, result[:errors])
+      else
+        @current_user = User.find_or_create_by!(uid: result[:uid])
+      end
     end
   end
 
   def current_user
-    @_current_user
+    @current_user
   end
 
 end
