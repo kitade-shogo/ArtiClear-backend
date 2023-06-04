c4f1662341c08d1f3f43ddea8cf7e60813785d42
kitadeshogo <tori3227@gmail.com>
2023-06-05 04:35:35 +0900

    Bookmarkに一意性制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2023_05_28_011537) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["folder_id"], name: "index_bookmarks_on_folder_id"
+    t.index ["url"], name: "index_bookmarks_on_url", unique: true
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
