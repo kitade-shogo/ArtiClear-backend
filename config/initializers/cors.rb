d891b2439a8516c9e83835182dd58202d3e1d683
kitadeshogo <tori3227@gmail.com>
2023-08-01 05:39:07 +0900

    cors設定の見直し

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://articlear.vercel.app', 'chrome-extension://bflmbecdlbonjancnhafkkpepmfgpkke'
+    origins 'https://articlear.vercel.app', 'https://articlear.jp', 'https://www.articlear.jp','chrome-extension://bflmbecdlbonjancnhafkkpepmfgpkke' #,'localhost:3000' 
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
